8c4fcfa06587310c05622a561ae5371cd35f0b3b
Samuel Souza <fsamuel.bs@gmail.com>
2015-10-30 00:32:42 -0200

    Screening fragment part. 2

--- firefighter/src/main/java/com/sigac/firefighter/ScreeningFragment.java
+++ firefighter/src/main/java/com/sigac/firefighter/ScreeningFragment.java
@@ -6,8 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ScreeningFragment extends Fragment {
+
+    private TextView mIdTextView;
 
     private Button mMaleButton;
     private Button mFemaleButton;
@@ -17,6 +20,11 @@ public class ScreeningFragment extends Fragment {
     private Button mAdultButton;
     private Button mOldButton;
 
+    private Button mSafeButton;
+    private Button mInjuredbutton;
+    private Button mSevereButton;
+    private Button mDeadButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,20 @@ public class ScreeningFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.screening_fragment, container, false);
+
+        mMaleButton = (Button) view.findViewById(R.id.button_male);
+        mFemaleButton = (Button) view.findViewById(R.id.button_female);
+
+        mChildButton = (Button) view.findViewById(R.id.button_child);
+        mYoungButton = (Button) view.findViewById(R.id.button_young);
+        mAdultButton = (Button) view.findViewById(R.id.button_male);
+        mOldButton = (Button) view.findViewById(R.id.button_male);
+
+        mSafeButton = (Button) view.findViewById(R.id.button_safe);
+        mInjuredbutton = (Button) view.findViewById(R.id.button_injured);
+        mSevereButton = (Button) view.findViewById(R.id.button_severe);
+        mDeadButton = (Button) view.findViewById(R.id.button_dead);
+
         return view;
     }
 }
