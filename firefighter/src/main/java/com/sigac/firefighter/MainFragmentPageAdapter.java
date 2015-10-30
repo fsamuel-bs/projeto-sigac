f43872961c575abe27ec3488fedc8014079f82d2
Bernardo Rufino <bermonruf@gmail.com>
2015-10-30 00:15:17 -0200

    Add guava and refactor adapter

--- firefighter/src/main/java/com/sigac/firefighter/MainFragmentPageAdapter.java
+++ firefighter/src/main/java/com/sigac/firefighter/MainFragmentPageAdapter.java
@@ -4,38 +4,42 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import com.google.common.base.Throwables;
 
 public class MainFragmentPageAdapter extends FragmentPagerAdapter {
 
-    final int PAGE_COUNT = 2;
-    private String tabTitles[] = new String[] { "Screening", "Search" };
+    // Who needs types? :P
+    private static Object[][] TABS = {
+            {"Screening", ScreeningFragment.class},
+            {"Search", SearchFragment.class},
+    };
 
-    private Context context;
+    private Context mContext;
 
     public MainFragmentPageAdapter(FragmentManager fm, Context context) {
         super(fm);
-        this.context = context;
+        mContext = context;
     }
 
     @Override
     public int getCount() {
-        return PAGE_COUNT;
+        return TABS.length;
     }
 
     @Override
     public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                return new ScreeningFragment();
-            case 1:
-                return new SearchFragment();
+        @SuppressWarnings("unchecked")
+        Class<? extends Fragment> klass = (Class<? extends Fragment>) TABS[position][1];
+        try {
+            return klass.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw Throwables.propagate(e);
         }
-        return null;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return tabTitles[position];
+        return (String) TABS[position][0];
     }
 
 }
