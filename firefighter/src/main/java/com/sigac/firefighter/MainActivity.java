b258c4a9aa6f0673e6703e463854baa3b6d30785
Bernardo Rufino <bermonruf@gmail.com>
2015-11-27 04:16:53 -0200

    Add simple action bar toggle behavior

--- firefighter/src/main/java/com/sigac/firefighter/MainActivity.java
+++ firefighter/src/main/java/com/sigac/firefighter/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,14 +14,23 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout mTabLayout;
     private View mSearchButton;
     private MainFragmentPageAdapter mAdapter;
+    private ViewGroup mActionBarContainer;
+    private View mSearchIcon;
+    private View mBackIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //getSupportActionBar().setHomeButtonEnabled(true);
-        mSearchButton = findViewById(R.id.main_search_icon);
+        mActionBarContainer = (ViewGroup) findViewById(R.id.action_bar_container);
+        setActionBar(R.id.action_bar_default);
+
+        mSearchIcon = findViewById(R.id.default_action_bar_search_icon);
+        mSearchIcon.setOnClickListener(mOnSearchIconClickListener);
+
+        mBackIcon = findViewById(R.id.search_action_bar_back_icon);
+        mBackIcon.setOnClickListener(mOnBackIconClickListener);
 
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mAdapter = new MainFragmentPageAdapter(getSupportFragmentManager(), this);
@@ -28,18 +38,31 @@ public class MainActivity extends AppCompatActivity {
 
         mTabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         mTabLayout.setupWithViewPager(mViewPager);
-
-        mSearchButton.setOnClickListener(mOnSearchButtonClickListener);
     }
 
-    private View.OnClickListener mOnSearchButtonClickListener = new View.OnClickListener() {
+    private View.OnClickListener mOnSearchIconClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Foo", Toast.LENGTH_LONG).show();
+            setActionBar(R.id.action_bar_search);
+            Toast.makeText(MainActivity.this, "Change fragment to search results", Toast.LENGTH_LONG).show();
         }
     };
 
-    /* TODO: LOL */
+    private View.OnClickListener mOnBackIconClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setActionBar(R.id.action_bar_default);
+            Toast.makeText(MainActivity.this, "Change fragment to previous one", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    private void setActionBar(int id) {
+        for (int i = 0; i < mActionBarContainer.getChildCount(); i++) {
+            View actionBar = mActionBarContainer.getChildAt(i);
+            actionBar.setVisibility((actionBar.getId() == id) ? View.VISIBLE : View.GONE);
+        }
+    }
+
     public void goToVictim(Victim victim) {
         mViewPager.setCurrentItem(MainFragmentPageAdapter.VICTIM_TAB_INDEX);
         VictimFragment fragment = (VictimFragment) mAdapter.getItem(MainFragmentPageAdapter.VICTIM_TAB_INDEX);
